c097ae9d59214e51f9780bd1e15765f1353f937a
ningmengchongshui <ningmengchongshui@gmail.com>
2025-01-07 19:11:59 +0800

    feat: 依赖接口

--- packages/admin/src/router.ts
+++ packages/admin/src/router.ts
@@ -1,5 +1,14 @@
 import koaRouter from 'koa-router'
-import { useState } from 'alemonjs'
+import { getConfig, useState } from 'alemonjs'
+// import { fileURLToPath } from 'url'
+// import { dirname, join } from 'path'
+import { readdirSync } from 'fs'
+import { join } from 'path'
+import { createRequire } from 'module'
+// const __dirname = dirname(fileURLToPath(import.meta.url))
+// const __data = join(__dirname, '../', 'data')
+
+const require = createRequire(import.meta.url)
 
 // 路由
 const router = new koaRouter({
@@ -11,24 +20,26 @@ router.get('/', async ctx => {
   ctx.body = 'Hello World'
 })
 
-// apps开关
-router.post('/apps/swich', async ctx => {
+// 获取功能开关
+router.get('/funtion/switch', async ctx => {
+  ctx.status = 200
+  ctx.body = 'ok'
+})
+
+// 修改功能开关
+router.post('/funtion/switch', async ctx => {
   const body = ctx.request.body as {
     name: string
     state: '1' | '0'
   }
   if (!body.name || body.name == '') {
-    ctx.body = {
-      code: 400,
-      message: 'name 不能为空'
-    }
+    ctx.status = 400
+    ctx.body = 'name 不能为空'
     return
   }
   if (body.state !== '1' && body.state !== '0') {
-    ctx.body = {
-      code: 400,
-      message: 'state 只能为 0 或 1'
-    }
+    ctx.status = 400
+    ctx.body = 'state 只能为 0 或 1'
     return
   }
   const [state, setState] = useState(body.name)
@@ -38,10 +49,53 @@ router.post('/apps/swich', async ctx => {
   if (!state && body.state == '1') {
     setState(true)
   }
+  ctx.status = 200
+  ctx.body = 'ok'
+})
+
+// 获取数据
+router.get('/config', async ctx => {
+  const cfg = getConfig()
+  ctx.status = 200
   ctx.body = {
-    code: 200,
-    message: 'success'
+    value: cfg.value,
+    package: cfg.package
   }
+})
+
+router.get('/node/modules', async ctx => {
+  // 得到 node_modules 的列表。去掉.开头的文件夹,去掉@types的文件夹
+  const nodeModules = join(process.cwd(), 'node_modules')
+  const files = readdirSync(nodeModules).filter(
+    file => !/^(@types|\.)/.test(file)
+  )
+  // .开头的文件夹
+  // @types 的文件夹
+  ctx.status = 200
+  ctx.body = files
+})
+
+router.post('/node/modules/package', async ctx => {
+  const body = ctx.request.body as {
+    names: string[]
+  }
+  if (!body.names || body.names.length == 0) {
+    ctx.status = 400
+    ctx.body = 'names 不能为空'
+    return
+  }
+  const packages = body.names.map(name => {
+    try {
+      return require(join(process.cwd(), 'node_modules', name, 'package.json'))
+    } catch (error) {
+      return {
+        name,
+        error: error.message
+      }
+    }
+  })
+  ctx.status = 200
+  ctx.body = packages
 })
 
 export default router
--- packages/admin/src/server.ts
+++ packages/admin/src/server.ts
@@ -1,10 +1,14 @@
 import koa from 'koa'
 import koaStatic from 'koa-static'
 import router from './router'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const __public = join(__dirname, '../', 'public')
 export const server = () => {
   // 挂静态资源。 public
   const app = new koa()
-  app.use(koaStatic(process.cwd() + '/public'))
+  app.use(koaStatic(__public))
   app.use(router.routes())
   app.use(router.allowedMethods())
   const port = 17137
